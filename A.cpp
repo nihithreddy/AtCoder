21e071f3e16502b87eb1d477e5095c1f741b4bfb
NihithReddy <sainihith9618@gmail.com>
2020-05-14 18:46:46 +0530

    A.cpp file added

---
+++ A.cpp
@@ -0,0 +1,15 @@
+﻿		//
+// Created by Nihith on 22-02-2020.
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int N,R;
+    cin>>N>>R;
+    if(N>=10) cout<<R<<endl;
+    else cout<<(R+100*(10-N))<<endl;
+    return 0;
+}
+
