d8ec355f2465e48a7146694eac8b64b453be9d7a
NihithReddy <sainihith9618@gmail.com>
2020-05-14 18:47:21 +0530

    B.cpp file added

---
+++ B.cpp
@@ -0,0 +1,18 @@
+﻿//
+// Created by Nihith on 22-02-2020.
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int N,K,ans=0;
+    cin>>N>>K;
+    while(N>0){
+        N/=K;
+        ans++;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
+
